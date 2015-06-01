2ce93cd811d822a44dc6664b172b604e0cbc5d0e
Evan Seils <evan@seils.cc>
2015-05-31 19:25:52 -0500

    Added light support to core

--- core.py
+++ core.py
@@ -2,64 +2,65 @@ from tornado import web, ioloop, escape
 import json as j
 import sys
 import os
+from light_control.lights import Lights
 
 
-#Python 2.7
-
-class Statue():
-    def __init__(self):
-        pass
-
-
+# Python 2.7
 
 class IndexHandler(web.RequestHandler):
     def get(self, *args, **kwargs):
-        #ToDo: Present login page if user is not authed
+        # ToDo: Present login page if user is not authed
         self.render("web/index.html", pages=jsa)
+
 
 jsn = {}
 jsa = []
+
+
 def startup():
-    #ToDo: hook up to statue
+
+    statue = Lights()
+
+    #statue.loadPattern()
 
     thedir = "plugins"
-    dirs = [ name for name in os.listdir(thedir) if os.path.isdir(os.path.join(thedir, name)) ]
+    dirs = [name for name in os.listdir(thedir) if os.path.isdir(os.path.join(thedir, name))]
 
-    pages = [(r'/', IndexHandler)] #we need to add every page to this, along with the class(es) that viewing the page executes.
-    
+    pages = [(r'/',
+              IndexHandler)]  # we need to add every page to this, along with the class(es) that viewing the page executes.
+
     for d in dirs:
 
         print(d)
         try:
-            json = j.loads(open("plugins/"+d+"/conf.json").read())
+            json = j.loads(open("plugins/" + d + "/conf.json").read())
         except Exception:
             continue
 
-        exec "import plugins."+d+".main"
+        exec "import plugins." + d + ".main"
 
         jsn[json['webname']] = json
         jsa.append(json)
         pgs = []
         for p in json['routes']:
-            pgs.append(("/"+json['webname']+p[0], eval("plugins."+d+".main."+p[1])))
-            #if
-            pgs.append(("/"+json['webname']+p[0]+"/([^/]+)", eval("plugins."+d+".main."+p[1])))
+            pgs.append(("/" + json['webname'] + p[0], eval("plugins." + d + ".main." + p[1])))
+            # if
+            pgs.append(("/" + json['webname'] + p[0] + "/([^/]+)", eval("plugins." + d + ".main." + p[1])))
 
         pages.extend(pgs)
 
     print(pages)
 
 
-    #start tornado sever
+    # start tornado sever
 
 
 
     app = web.Application(pages, debug=True)
     app.listen(9893)
-    ioloop.IOLoop.instance().start()  #The program will never get past this. Put all code above.
+    ioloop.IOLoop.instance().start()  # The program will never get past this. Put all code above.
 
     pass
-
 
 
 startup()
