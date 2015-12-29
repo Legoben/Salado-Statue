8cc80c0dd67bb7a5c2c2322a54661b772308ffd5
Evan Seils <evanseils2013@gmail.com>
2015-12-28 18:59:12 -0600

    Add light class to core file

--- core.py
+++ core.py
@@ -2,7 +2,8 @@ from tornado import web, ioloop, escape
 import json as j
 import sys
 import os
-from light_control.lights import Lights
+from time import sleep
+import RPi.GPIO as GPIO
 
 
 # Python 2.7
@@ -16,6 +17,62 @@ class IndexHandler(web.RequestHandler):
 jsn = {}
 jsa = []
 
+class Lights():
+    # GPIO pins
+    serial_pin = 23
+    clock_pin = 24
+    latch_pin = 25
+
+
+    # cleanup and init GPIO pins when class is created
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.serial_pin, GPIO.OUT)
+        GPIO.setup(self.clock_pin, GPIO.OUT)
+        GPIO.setup(self.latch_pin, GPIO.OUT)
+
+    # clock bit
+    def clock(self):
+        GPIO.output(self.clock_pin, GPIO.HIGH)
+        sleep(0.01)
+        GPIO.output(self.clock_pin, GPIO.LOW)
+        sleep(0.01)
+
+    # set 16 bits to parallel output on the shift register
+    def latch(self):
+        GPIO.output(self.latch_pin, GPIO.HIGH)
+        GPIO.output(self.latch_pin, GPIO.LOW)
+
+    # Convert Hexadecimal input to Binary
+    def parsehex(array):
+        for hex in range(len(array)):
+            array[hex] = bin(int(array[hex], 16))[2:]
+            array[hex] = [int(char) for char in array[hex]]
+        return array
+
+    # load pattern from multi-array
+    def loadPattern(self, pattern, delay, hexinput=True):
+        if hexinput:
+            pattern = parsehex(pattern)
+        GPIO.output(self.latch_pin, GPIO.LOW)
+        for subarray_count in range(len(pattern)):
+            for i in range(24):
+                GPIO.output(self.serial_pin, pattern[subarray_count][i])
+                self.clock()
+                print "\033[92mShifted In: " + str(pattern[subarray_count]) + "\033[0m"
+            sleep(delay)
+            self.latch()
+
+    # turn on a single light   Ex. light(12) - Turns on light number 12
+    def light(self, lightNum):
+        sequence = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+        sequence[lightNum] = 1
+
+        self.loadPattern([sequence], 0)
+
+    def __del__(self):
+        GPIO.cleanup()
 
 def startup():
 
